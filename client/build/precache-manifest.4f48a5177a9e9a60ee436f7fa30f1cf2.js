5f80c6893fdc58251b4ef67c59c7fc291bd9372f
srinath3280 <145562048+srinath3280@users.noreply.github.com>
2024-10-30 13:16:13 +0530

    30-10-2024

---
+++ client/build/precache-manifest.4f48a5177a9e9a60ee436f7fa30f1cf2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd5de222106bb1011a8a",
+    "url": "/static/css/main.82b80879.chunk.css"
+  },
+  {
+    "revision": "bd5de222106bb1011a8a",
+    "url": "/static/js/main.85a4882e.chunk.js"
+  },
+  {
+    "revision": "5ede8e2b1782d00c453b",
+    "url": "/static/js/runtime~main.8f3b8d9d.js"
+  },
+  {
+    "revision": "27cc5e07e6d36b920e4f",
+    "url": "/static/js/xlsx.9bad6e48.chunk.js"
+  },
+  {
+    "revision": "0d840515c2e71956d774",
+    "url": "/static/css/3.c886a5b9.chunk.css"
+  },
+  {
+    "revision": "0d840515c2e71956d774",
+    "url": "/static/js/3.ae77d069.chunk.js"
+  },
+  {
+    "revision": "49134f906cbd305bfee0",
+    "url": "/static/js/4.948a2892.chunk.js"
+  },
+  {
+    "revision": "cc25e892e3d1239ba70e",
+    "url": "/static/js/5.54906687.chunk.js"
+  },
+  {
+    "revision": "5a6dbadd5ab16ee71d1b",
+    "url": "/static/js/6.62993a4a.chunk.js"
+  },
+  {
+    "revision": "35a0a4daa858ec8cd59423d4f2096c0c",
+    "url": "/index.html"
+  }
+];
